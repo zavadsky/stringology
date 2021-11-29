a35a039b0bec82a20aa46fb142b6b958203c5711
zavadsky <zava@ukr.net>
2021-11-29 23:00:32 +0200

    Create Bricks28k_DNA.cpp

---
+++ Bricks/Bricks for DNA & Protein/Bricks28k_DNA.cpp
@@ -0,0 +1,37 @@
+int Bricks28_DNA(unsigned char *P, int m, unsigned char *T, int n,int k=14) {
+    int mp1=m+1, mm1=m-1, mm2=m-2, mm3=m-3, mm4=m-4, mm5=m-5, mm6=m-6, mm7=m-7,mm8=m-8, r, count = 0;
+	const unsigned int _2_power_k=(1<<k),mask=_2_power_k-1;
+    unsigned char *Z=(unsigned char *)calloc(_2_power_k,sizeof(unsigned char));
+    unsigned char *s, *stop;
+    int QS[256];
+    if(m<=7)
+        return -1;
+
+    //Preprocessing
+    for(int i=0;i<256;i++)				        // QS shift table
+        QS[i]=mp1;
+    for(int i=0;i<m;i++)
+        QS[P[i]]=m-i;
+    uint32_t p=*(uint32_t*)P;
+    for (int i = 0; i < _2_power_k; i++)
+		Z[i] = 1;
+    for(int i=0;i<mm6;i++)
+        Z[((P[i]>>1)+(P[i+1]<<1)+(P[i+2]<<3)+(P[i+3]<<5)+(P[i+4]<<7)+(P[i+5]<<9)+(P[i+6]<<11)+(P[i+7]<<13))&mask]=0;
+
+   // Searching
+   s = T+m-8;       //current position
+   stop = T+n-7;
+   count = 0;
+   memcpy(T+n,P,m);
+   while(s<stop) {
+        while(Z[((*s>>1)+(*(s+1)<<1)+(*(s+2)<<3)+(*(s+3)<<5)+(*(s+4)<<7)+(*(s+5)<<9)+(*(s+6)<<11)+(*(s+7)<<13))&mask])
+            s+=mm7;
+        if(*(uint32_t*)(s-mm8)==p) {   // occurrence check;
+            for (r = 4; r < m && s[r-mm8] == P[r]; r++);
+            if (r == m && s<stop)
+                count++;
+        }
+        s+=QS[*(s+8)];
+    }
+	return count;
+}
