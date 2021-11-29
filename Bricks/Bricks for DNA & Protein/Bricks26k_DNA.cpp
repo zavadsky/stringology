ea8b29a9b18dc55e7d6494c2fbfee59dc40e6c18
zavadsky <zava@ukr.net>
2021-11-29 22:40:35 +0200

    Update Bricks26k_DNA.cpp

--- Bricks/Bricks for DNA & Protein/Bricks26k_DNA.cpp
+++ Bricks/Bricks for DNA & Protein/Bricks26k_DNA.cpp
@@ -1,7 +1,7 @@
 int Bricks26_DNA(unsigned char *P, int m, unsigned char *T, int n,int k=14) {
     int mp1=m+1, mm1=m-1, mm2=m-2, mm3=m-3, mm4=m-4, mm5=m-5, mm6=m-6, r, count = 0;
-	  const unsigned int _2_power_k=(1<<k),mask=_2_power_k-1,_2_power_k_2=_2_power_k*2;
-    unsigned char *Z=(unsigned char *)calloc(_2_power_k_2,sizeof(unsigned char));
+	  const unsigned int _2_power_k=(1<<k),mask=_2_power_k-1;
+    unsigned char *Z=(unsigned char *)calloc(_2_power_k,sizeof(unsigned char));
     unsigned char *s, *stop;
     int QS[256];
     if(m<=5)
