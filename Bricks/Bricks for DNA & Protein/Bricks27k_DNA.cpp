add13baabd26a607b91b1adf84ba54b63210e8ab
zavadsky <zava@ukr.net>
2021-11-29 22:43:30 +0200

    Update Bricks27k_DNA.cpp

--- Bricks/Bricks for DNA & Protein/Bricks27k_DNA.cpp
+++ Bricks/Bricks for DNA & Protein/Bricks27k_DNA.cpp
@@ -1,7 +1,7 @@
 int Bricks27_DNA(unsigned char *P, int m, unsigned char *T, int n,int k=14) {
     int mp1=m+1, mm1=m-1, mm2=m-2, mm3=m-3, mm4=m-4, mm5=m-5, mm6=m-6, mm7=m-7, r, count = 0;
-	const unsigned int _2_power_k=(1<<k),mask=_2_power_k-1,_2_power_k_2=_2_power_k*2;
-    unsigned char *Z=(unsigned char *)calloc(_2_power_k_2,sizeof(unsigned char));
+	const unsigned int _2_power_k=(1<<k),mask=_2_power_k-1;
+    unsigned char *Z=(unsigned char *)calloc(_2_power_k,sizeof(unsigned char));
     unsigned char *s, *stop;
     int QS[256];
     if(m<=6)
@@ -13,7 +13,7 @@ int Bricks27_DNA(unsigned char *P, int m, unsigned char *T, int n,int k=14) {
     for(int i=0;i<m;i++)
         QS[P[i]]=m-i;
     uint32_t p=*(uint32_t*)P;
-    for (int i = 0; i < _2_power_k_2; i++)
+    for (int i = 0; i < _2_power_k; i++)
 		Z[i] = 1;
     for(int i=0;i<mm5;i++)
         Z[((P[i]>>1)+(P[i+1]<<1)+(P[i+2]<<3)+(P[i+3]<<5)+(P[i+4]<<7)+(P[i+5]<<9)+(P[i+6]<<11))&mask]=0;
