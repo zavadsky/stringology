54183611823c7c6a05552c4c8df1511e4d0623e3
zavadsky <zava@ukr.net>
2021-11-30 07:24:49 +0200

    Update Bricks_25_16_1_DNA.cpp

--- Bricks/Bricks for DNA/Bricks_25_16_1_DNA.cpp
+++ Bricks/Bricks for DNA/Bricks_25_16_1_DNA.cpp
@@ -1,4 +1,4 @@
-int Bricks_25_1_16_DNA(unsigned char *P, int m, unsigned char *T, int n) {
+int Bricks_25_1_DNA(unsigned char *P, int m, unsigned char *T, int n) {
     int mp1=m+1, mm1=m-1, mm2=m-2, mm3=m-3, mm4=m-4, mm5=m-5, r, count = 0;
 	  const unsigned int _2_power_k=(1<<17);
 	  unsigned char *Z=(unsigned char *)calloc(_2_power_k,sizeof(unsigned char)),Alph[4]={'A','C','G','T'};
