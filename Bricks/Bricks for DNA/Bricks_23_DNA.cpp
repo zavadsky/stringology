50f28f11a64b869c04ad00d20391c63ce74c2665
zavadsky <zava@ukr.net>
2021-11-30 07:26:15 +0200

    Update and rename Bricks_23_16_DNA.cpp to Bricks_23_DNA.cpp

--- Bricks/Bricks for DNA/Bricks_23_DNA.cpp
+++ Bricks/Bricks for DNA/Bricks_23_DNA.cpp
@@ -1,4 +1,4 @@
-int Bricks23_16_DNA(unsigned char *P, int m, unsigned char *T, int n) {
+int Bricks23_DNA(unsigned char *P, int m, unsigned char *T, int n) {
     int mp1=m+1, mm1=m-1, mm2=m-2, mm3=m-3, mm4=m-4, m2=m*2, r, count = 0;
 	  const unsigned int _2_power_k=(1<<16);
     unsigned char *Z=(unsigned char *)calloc(_2_power_k,sizeof(unsigned char));
